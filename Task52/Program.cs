1902685bcc7bb64b3086bf1422c40d00905e6d99
EkaterinaBildina <kukresch@gmail.com>
2023-01-24 14:56:27 +0300

    Task52 Program.cs success

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+/*
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+
+
+
+double[] AverageColumnsNumbers(int[,] matrix)
+{
+    double[] AverageColumns = new double[matrix.GetLength(1)];
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        int length = matrix.GetLength(0);
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum = sum + matrix[i, j];
+            double average = sum / length;
+            AverageColumns[j] = Math.Round(average, 1);
+        }
+       
+    }
+    return AverageColumns;
+}
+
+void PrintMas(double[] mas)
+{
+    Console.Write("[");
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (i < mas.Length - 1) Console.Write($"{Math.Round(mas[i],1),5};");
+        else Console.Write($"{Math.Round(mas[i],1),5}  ");
+    }
+    Console.Write("]");
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 0, 10);
+PrintMatrix(array2D);
+
+double[] arrayAverage = AverageColumnsNumbers(array2D);
+Console.WriteLine("Average columns result:");
+PrintMas(arrayAverage);
+
